7740b17c2aad330a3eddb03509bc1c0f0a6bcf8b
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-05-10 16:28:23 +0530

    Add Hero Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import HeroSection from "./components/hero-section";
 import NavBar from "./components/nav-bar";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<NavBar />
+			<HeroSection />
 		</>
 	);
 }
---
+++ src/components/hero-section.tsx
@@ -0,0 +1,33 @@
+import "./hero-section.scss";
+
+function HeroSection() {
+	return (
+		<div className="hero-section">
+			<img src="/background.png" alt="FOCus Night Background" className="background" />
+
+			<div className="content">
+				<span className="title">TIME REMAINNG</span>
+				<div className="count-down">
+					<div className="block">
+						<div className="count">00</div>
+						<span>DAYS</span>
+					</div>
+					<div className="block">
+						<div className="count">00</div>
+						<span>HOURS</span>
+					</div>
+					<div className="block">
+						<div className="count">00</div>
+						<span>MINUTES</span>
+					</div>
+					<div className="block">
+						<div className="count">00</div>
+						<span>SECONDS</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default HeroSection;
