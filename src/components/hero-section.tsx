0157da8618e41ac68eb2bff58adf38c6b36adb2c
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-05-10 17:24:16 +0530

    Add countdown functionalty

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
 import "./hero-section.scss";
 
+const countDownDate = new Date("May 25, 2024 08:00:00").getTime();
+
 function HeroSection() {
+	const [day, setDay] = useState({
+		days: 0,
+		hours: 0,
+		minutes: 0,
+		seconds: 0,
+	});
+
+	function set() {
+		const now = new Date().getTime();
+		const distance = countDownDate - now;
+		const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		setDay({ days, hours, minutes, seconds });
+	}
+
+	useEffect(() => {
+		set();
+		const interval = setInterval(() => {
+			set();
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
 		<div className="hero-section">
 			<div className="background"></div>
@@ -9,19 +38,19 @@ function HeroSection() {
 				<span className="title">TIME REMAINNG</span>
 				<div className="count-down">
 					<div className="block">
-						<div className="count">00</div>
+						<div className="count">{day.days}</div>
 						<span>DAYS</span>
 					</div>
 					<div className="block">
-						<div className="count">00</div>
+						<div className="count">{day.hours}</div>
 						<span>HOURS</span>
 					</div>
 					<div className="block">
-						<div className="count">00</div>
+						<div className="count">{day.minutes}</div>
 						<span>MINUTES</span>
 					</div>
 					<div className="block">
-						<div className="count">00</div>
+						<div className="count">{day.seconds}</div>
 						<span>SECONDS</span>
 					</div>
 				</div>
