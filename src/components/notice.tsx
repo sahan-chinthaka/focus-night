e4a89f938ad8615dcd23d9c88038898e422a2179
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-05-10 18:55:35 +0530

    Add notice carousel

--- src/components/notice.tsx
+++ src/components/notice.tsx
@@ -1,9 +1,45 @@
 import "./notice.scss";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+	superLargeDesktop: {
+		breakpoint: { max: 4000, min: 3000 },
+		items: 5,
+	},
+	desktop: {
+		breakpoint: { max: 3000, min: 1024 },
+		items: 3,
+	},
+	tablet: {
+		breakpoint: { max: 1024, min: 464 },
+		items: 2,
+	},
+	mobile: {
+		breakpoint: { max: 464, min: 0 },
+		items: 1,
+	},
+};
 
 function Notice() {
 	return (
 		<div className="notice" id="notice">
 			<p className="title">NOTICES</p>
+
+			<Carousel responsive={responsive}>
+				<div className="item">
+					<img src="/notices/notice1.jpeg" alt="" />
+				</div>
+				<div className="item">
+					<img src="/notices/notice1.jpeg" alt="" />
+				</div>
+				<div className="item">
+					<img src="/notices/notice1.jpeg" alt="" />
+				</div>
+				<div className="item">
+					<img src="/notices/notice1.jpeg" alt="" />
+				</div>
+			</Carousel>
 		</div>
 	);
 }
