74e6049a4c43b76ed5ddd8a6a957e89a413c57b2
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-05-10 20:01:36 +0530

    Change contact details

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -6,19 +6,20 @@ function Contact() {
 			<p className="title">CONTACT</p>
 			<div className="profiles">
 				<div className="profile">
-					<img src="/background.png" alt="" />
+					<div className="profile-img">
+						<img src="/president.jpg" alt="" />
+					</div>
 					<p className="name">Dinuka</p>
-					<p className="number">CONTACT: 077-4942166</p>
+					<p className="post">President</p>
+					<p className="number">CONTACT: 077-0162082</p>
 				</div>
 				<div className="profile">
-					<img src="/background.png" alt="" />
-					<p className="name">Dinuka</p>
-					<p className="number">CONTACT: 077-4942166</p>
-				</div>
-				<div className="profile">
-					<img src="/background.png" alt="" />
-					<p className="name">Dinuka</p>
-					<p className="number">CONTACT: 077-4942166</p>
+					<div className="profile-img">
+						<img src="/vice-president.jpg" alt="" />
+					</div>
+					<p className="name">Kaveesha</p>
+					<p className="post">Vice President</p>
+					<p className="number">CONTACT: 076-6365729</p>
 				</div>
 			</div>
 		</div>
