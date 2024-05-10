afa2342062bf98e88429ea3484a461e0e7ca47a8
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-05-10 18:44:05 +0530

    Add notices

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import HeroSection from "./components/hero-section";
 import NavBar from "./components/nav-bar";
+import Notice from "./components/notice";
 
 function App() {
 	return (
 		<>
 			<NavBar />
 			<HeroSection />
+			<Notice />
 		</>
 	);
 }
---
+++ src/components/notice.tsx
@@ -0,0 +1,11 @@
+import "./notice.scss";
+
+function Notice() {
+	return (
+		<div className="notice" id="notice">
+			<p className="title">NOTICES</p>
+		</div>
+	);
+}
+
+export default Notice;
