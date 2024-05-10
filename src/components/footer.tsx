36370225b2d2544b03aa57de74d83e88559c6f24
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-05-10 20:01:28 +0530

    Change footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,9 @@ import "./footer.scss";
 function Footer() {
 	return (
 		<div className="footer">
-			<p>© FACULTY OF COMPUTING</p>
+			<p>
+				© STUDENTS' UNION OF FACULTY OF COMPUTING
+			</p>
 		</div>
 	);
 }
